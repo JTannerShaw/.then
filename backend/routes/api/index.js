901777d43a1421df066a6953260546edcbe3cc16
Tanner Shaw <traumaaa@gmail.com>
2022-01-06 13:35:53 -0800

    Added user signup validations

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -16,22 +16,4 @@ router.post('/test', function(req, res) {
 
 
 
-
 module.exports = router;
-// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-//   const user = await User.findOne({
-//     where: {
-//       username: 'Demo-user'
-//     }
-//   })
-//   setTokenCookie(res, user);
-//   return res.json({ user });
-// }))
-
-// router.get('/restore-user', restoreUser, (req, res) => {
-//   return res.json(req.user);
-// })
-
-// router.get('/require-auth', requireAuth, (req, res) => {
-//   return res.json(req.user);
-// })

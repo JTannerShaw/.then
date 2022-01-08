a8bb4de1dfddca5e51d54b29c5465ed8406a6132
Tanner Shaw <traumaaa@gmail.com>
2022-01-07 16:39:13 -0800

    Stupid CSS

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import './Footer.css'
 function Footer() {
   return (
     <div className='footer'>
-      Copyright 2022. <a href='https://github.com/JTannerShaw'>GitHub</a> <a href='https://www.linkedin.com/in/tanner-shaw-a25702162/'>LinkedIn</a>
+      <p className='cc'>Copyright 2022.</p> <a className='github-a' href='https://github.com/JTannerShaw'>GitHub</a> <a className='linkedin-a' href='https://www.linkedin.com/in/tanner-shaw-a25702162/'>LinkedIn</a>
     </div>
   )
 }

2d854b5bab3ba3c44e98c748e0b0f149787041f3
Tanner Shaw <traumaaa@gmail.com>
2022-01-13 14:44:06 -0800

    Answer fetching done

--- frontend/src/components/QuestionDetail/index.js
+++ frontend/src/components/QuestionDetail/index.js
@@ -3,21 +3,30 @@ import { NavLink, useHistory, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
 import { getAllQuestions, deleteQuestion } from "../../store/question";
 import * as sessionActions from '../../store/session';
+import * as answerActions from '../../store/answer';
 import './QuestionDetail.css'
 
 const QuestionDetail = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const questionId = useParams();
-
+console.log(questionId)
   const { id } = questionId;
   const questions = useSelector(state => state.question.entries);
   const sessionUser = useSelector(state => state.session.user);
+  const answers = useSelector(state => state.answer.entries);
+  // const users = useSelector(state => state.session.entries)
   const choice = questions.find(question => question.id === +id)
+
+  const questionsAnswers = answers.filter(answer => answer.questionId === +id);
+
+
 
   useEffect(() => {
     window.scrollTo(0, 0);
     dispatch(getAllQuestions())
+    dispatch(sessionActions.getAllUsers())
+    dispatch(answerActions.getAllAnswers())
     dispatch(sessionActions.restore())
   }, [dispatch]);
 
@@ -33,6 +42,12 @@ const QuestionDetail = () => {
     history.push('/');
   }
 
+
+  const handleAnswerDelete = async (e) => {
+    e.preventDefault();
+    await dispatch(answerActions.deleteAnswer(choice))
+  }
+
   return (
     <div className='question-detail-container-wrapper'>
       <div className='question-detail-wrapper'>
@@ -44,6 +59,18 @@ const QuestionDetail = () => {
           {sessionUser?.id === choice.ownerId ? <button className="delete-button" onClick={handleDelete} type='submit'>Delete Question</button> : <></>}
         </div>
       </div>
+      {questionsAnswers && questionsAnswers.map((answer) => {
+        return (
+          <div className='answer-detail-wrapper'>
+          <div className='answer-detail-container'>
+            <p className=''>{answer.answer}</p>
+            <p className='answered-by'></p>
+            {sessionUser?.id === answer.userId ? <NavLink className='edit-button' to={`/questions/${id}/edit`}>Edit</NavLink> : <></>}
+            {sessionUser?.id === answer.userId ? <button className="delete-button" onClick={handleDelete} type='submit'>Delete Question</button> : <></>}
+            </div>
+            </div>
+        )
+      })}
       <div className='related-questions-container'>
         <h2 className='related-questions-title'>Related Questions</h2>
         {questions && questions.map((question) => {
